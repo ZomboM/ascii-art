2dc0423e345f13f56c6b065b04d0ba3b6066d5e9
nicolaus <nicolaus.maloney@gmail.com>
2023-03-07 20:38:27 -0500

    added colors

--- main.js
+++ main.js
@@ -12,14 +12,16 @@ const gradient = [
   '#',
   '@',
 ]
-gradient.reverse()
 
 const getPixelAt = (img, x, y) =>
   Jimp.intToRGBA(img.getPixelColor(x, y))
 
-const getLargePixel = (img, x, y, w, h) => {
+const getLargePixel = (img, x, y, w, h, color=false) => {
   let size = w * h
   let total = 0
+  let tr = 0
+  let tg = 0
+  let tb = 0
   img.scan(x, y, w, h, (x, y, idx) => {
     let r = img.bitmap.data[idx]
     let g = img.bitmap.data[idx + 1]
@@ -28,11 +30,14 @@ const getLargePixel = (img, x, y, w, h) => {
     let av = (0.21 * r + 0.72 * g + 0.07 * b) + 255 - a
     if (av > 255) av = 255
     total += av
+    tr += r
+    tg += g
+    tb += b
   })
-  return total / size
+  return color ? [total / size, tr / size, tg / size, tb / size].map(x => Math.floor(x)) : total / size
 }
 
-const generateASCII = (img, w, h) => {
+const generateASCII = (img, w, h, color=false) => {
   let imgW = img.bitmap.width
   let imgH = img.bitmap.height
   let widths = Array(w).fill(null)
@@ -54,30 +59,43 @@ const generateASCII = (img, w, h) => {
   let string = ''
   heights.forEach((h_, i) => {
     widths.forEach((w_, j) => {
-      let brightness =
-        getLargePixel(img, totalW, totalH, w_, h_)
+      if (!color) {
+        let brightness =
+          getLargePixel(img, totalW, totalH, w_, h_, false)
+        brightness =
+          Math.floor(brightness * gradient.length / 256)
+        string += gradient[brightness]
+        string += gradient[brightness]
+      } else {
+        let data =
+          getLargePixel(img, totalW, totalH, w_, h_, true)
+        let brightness = data[0]
+        string += `\x1b[38;2;${data[1]};${data[2]};${data[3]}m`
+        brightness =
+          Math.floor(brightness * gradient.length / 256)
+        string += gradient[brightness]
+        string += gradient[brightness]
+      }
       totalW += w_
-      brightness =
-        Math.floor(brightness * gradient.length / 256)
-      string += gradient[brightness]
-      string += gradient[brightness]
     })
     totalW = 0
     totalH += h_
     string += '\n'
   })
-  return string
+  return string + (color ? '\x1b[0m' : '')
 }
 
 class ASCII {
   constructor(gradient_=gradient) {
-    this.gradient = gradient_
+    if (gradient_ == 'lbg') this.gradient = [...gradient].reverse()
+    else if (gradient == 'dbg') this.gradient = gradient
+    else this.gradient = gradient_
   }
-  async generate(img_, width=32, height=32) {
+  async generate(img_, width=32, height=32, color=false) {
     let img
     if (img_ instanceof Jimp) img = img_
     else if (typeof img_ == 'string') img = await Jimp.read(img_)
     else throw new Error('Error: Image must either be a Jimp object or a string pointing to a file')
-    return generateAscii(img, width, height)
+    return generateASCII(img, width, height, color)
   }
 }
